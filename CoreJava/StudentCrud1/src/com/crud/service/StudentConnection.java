acac1ba15c5e31a9f85a9ab4c8e0b59549c320ef
niitagile <43024343+niitagile@users.noreply.github.com>
2022-09-02 11:01:55 +0000

    Add files via upload

--- CoreJava/StudentCrud1/src/com/crud/service/StudentConnection.java
+++ CoreJava/StudentCrud1/src/com/crud/service/StudentConnection.java
@@ -15,4 +15,44 @@ import java.sql.DriverManager;
  */
 public class StudentConnection {
 	
+	/*public static Connection getConnection(){
+		Connection con=null;
+		//Step 1 : load Driver in memory
+				try {
+					//to call Class loader to load class in memory at dymanic time
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					
+					
+				
+				
+					//Step 2: Database information 
+				con=DriverManager.getConnection("jdbc:mysql://localhost:3306/cred", "root","Comnet@123");
+				} catch (Exception e) {
+					
+					e.printStackTrace();
+				}
+				
+				return con;
+	}*/
+	public static Connection getConnection(){
+		Connection con=null;
+		//Step 1 : load Driver in memory
+				try {
+					//to call Class loader to load class in memory at dymanic time
+					Class.forName("oracle.jdbc.OracleDriver");
+					
+					
+				
+				
+					//Step 2: Database information 
+				con=DriverManager.getConnection("jdbc:oracle:thin@localhost:1521:orcl", "hr","hr1");
+				//for remote oracle
+				//con=DriverManager.getConnection("jdbc:oracle:thin@//192.32.78.90:1521:orcl", "hr","hr1");
+				} catch (Exception e) {
+					
+					e.printStackTrace();
+				}
+				
+				return con;
+	}
 }
--- CoreJava/StudentCrud1/src/com/crud/service/StudentService.java
+++ CoreJava/StudentCrud1/src/com/crud/service/StudentService.java
@@ -9,41 +9,87 @@ import java.util.List;
 
 import com.crud.model.Student;
 
-public class StudentService implements StudentCrud{
+public class StudentService implements StudentCrud {
 
 	@Override
 	public int insert(Student student) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		try {
+			Connection con = StudentConnection.getConnection();
+			java.sql.Date sdob=new java.sql.Date(student.getDob().getTime());
+			String sql="insert into studentDetails(rollno, studname,dob) values(?,?,?)";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setInt(1, student.getRollno());
+			ps.setString(2, student.getStudname());
+			ps.setDate(3, sdob);
+			
+			result=ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public int update(Student student) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		try {
+			Connection con = StudentConnection.getConnection();
+			java.sql.Date sdob=new java.sql.Date(student.getDob().getTime());
+			String sql="update studentDetails set studname=?,dob=? where rollno=?";;
+			PreparedStatement ps=con.prepareStatement(sql);
+			
+			ps.setString(1, student.getStudname());
+			ps.setDate(2, sdob);
+			ps.setInt(3, student.getRollno());
+			
+			result=ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public int delete(int rollno) {
-		// TODO Auto-generated method stub
-		return 0;
+
+		int result = 0;
+		try {
+			Connection con = StudentConnection.getConnection();
+			String sql="delete from studentdetails where rollno=?";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ps.setInt(1, rollno);
+			result=ps.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@Override
 	public List<Student> getAllRecords() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Student> list = new ArrayList<>();
+		try {
+			Connection con = StudentConnection.getConnection();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 	@Override
 	public Student getStudentByRollno(int rollno) {
-		// TODO Auto-generated method stub
-		return null;
+		Student student = null;
+		try {
+			Connection con = StudentConnection.getConnection();
+			String sql="";
+			PreparedStatement ps=con.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return student;
 	}
-	
-
-	
-
-
 
 }
