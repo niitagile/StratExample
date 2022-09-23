83ada642e304271c14b7cf813416eaad6271923d
niitagile <43024343+niitagile@users.noreply.github.com>
2022-09-23 12:06:21 +0000

    Add files via upload

---
+++ Hibernate/Log4jExample/src/com/example/StoreData.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+public class StoreData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Configuration cfg=new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory factory=cfg.buildSessionFactory();
+		Session session=factory.openSession();
+		Transaction tx=session.beginTransaction();
+		
+		session.persist(new Employee("Arun",380000));
+		
+		tx.commit();
+		session.close();
+	}
+
+}
