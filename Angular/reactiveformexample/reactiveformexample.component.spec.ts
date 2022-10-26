f271c327d8f2d152504334dee735b4e7b98c7913
niitagile <43024343+niitagile@users.noreply.github.com>
2022-10-26 09:15:31 +0000

    Add files via upload

---
+++ Angular/reactiveformexample/reactiveformexample.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReactiveformexampleComponent } from './reactiveformexample.component';
+
+describe('ReactiveformexampleComponent', () => {
+  let component: ReactiveformexampleComponent;
+  let fixture: ComponentFixture<ReactiveformexampleComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReactiveformexampleComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReactiveformexampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
